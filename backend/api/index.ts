0a50e556465e0c8c25aad3d221337c72e6228476
Calummullen <calummullen1995@gmail.com>
2025-01-20 15:16:17 +0000

    Update index.ts

--- backend/api/index.ts
+++ backend/api/index.ts
@@ -64,14 +64,14 @@ app.post("/api/users", async (req: Request, res: Response) => {
     .cookie("accessToken", accessToken, {
       httpOnly: true,
       secure: isProduction,
-      domain: isProduction ? ".diamedic.co.uk" : "localhost",
+      domain: ".diamedic.co.uk",
       sameSite: "none",
       maxAge: 9000,
     })
     .cookie("refreshToken", refreshToken, {
       httpOnly: true,
       secure: isProduction,
-      domain: isProduction ? ".diamedic.co.uk" : "localhost",
+      domain: ".diamedic.co.uk",
       sameSite: "none",
       maxAge: 604800000,
     })
