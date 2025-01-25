58afdd0dccbe96f82a336b881723e8910d4ceb23
Calummullen <calummullen1995@gmail.com>
2025-01-25 19:36:55 +0000

    fix stuff

--- backend/src/helpers/encryption.ts
+++ backend/src/helpers/encryption.ts
@@ -1,18 +1,10 @@
 import crypto from "crypto";
-import jwt from "jsonwebtoken";
 import dotenv from "dotenv";
-import { NextFunction } from "express";
-import { Request, Response } from "express";
 
 dotenv.config();
 
 const ENCRYPTION_KEY = process.env.ENCRYPTION_KEY!; // Must be 32 bytes
 const IV_LENGTH = 16; // AES block size (must be 16 bytes)
-
-const SECRET = process.env.JWT_SECRET!;
-const REFRESH_SECRET = process.env.JWT_REFRESH_SECRET!;
-const ACCESS_TOKEN_EXPIRY = "15m";
-const REFRESH_TOKEN_EXPIRY = "7d";
 
 /**
  * Encrypts the user data before storing it in Firestore
@@ -49,42 +41,3 @@ export const decryptData = (encryptedData: string): any => {
 
   return JSON.parse(decrypted);
 };
-
-/**
- * Generate JWT Tokens
- */
-export const generateTokens = (userId: string) => {
-  const accessToken = jwt.sign({ userId }, SECRET, {
-    expiresIn: ACCESS_TOKEN_EXPIRY,
-  });
-  const refreshToken = jwt.sign({ userId }, REFRESH_SECRET, {
-    expiresIn: REFRESH_TOKEN_EXPIRY,
-  });
-  return { accessToken, refreshToken };
-};
-
-/**
- * Middleware to Verify Access Token
- */
-export const verifyToken = async (
-  req: Request,
-  res: Response,
-  next: NextFunction
-) => {
-  console.log("INSIDE VERIFY TOKEN", req.cookies);
-  const token = req.cookies.accessToken;
-
-  if (!token)
-    return res.status(401).json({ error: "Unauthorized: No token provided" });
-
-  try {
-    const decoded = jwt.verify(token, SECRET) as { userId: string };
-    req.user = decoded;
-    next();
-  } catch (err) {
-    return res.status(403).json({ error: "Unauthorized: Invalid token" });
-  }
-};
-
-export const generateShortLivedToken = (userId: string) =>
-  jwt.sign({ userId }, SECRET, { expiresIn: "5m" }); // 5-minute expiration
