0251d5c431ac7424bbf3e80b9f1604232df273b0
Calummullen <calummullen1995@gmail.com>
2025-02-02 21:43:41 +0000

    JUST WORK!! >:(

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -3,10 +3,63 @@ import cors from "cors";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import userRoutes from "./routes/userRoute";
+import Stripe from "stripe";
+import { sendOrderConfirmationEmail } from "./services/emailService";
+import bodyParser from "body-parser";
 
 dotenv.config();
 
 const app = express();
+
+app.post(
+  "/webhook",
+  express.raw({ type: "application/json" }),
+  async (request, response) => {
+    const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!);
+    const sig = request.headers["stripe-signature"];
+    try {
+      if (!sig) {
+        return response.status(400).send("Missing Stripe signature");
+      }
+      let event;
+      try {
+        event = stripe.webhooks.constructEvent(
+          request.body,
+          sig,
+          process.env.STRIPE_WEBHOOK_SECRET!
+        );
+      } catch (error) {
+        console.error(
+          "Stripe Signature Verification Failed:",
+          (error as Error).message
+        );
+        return response
+          .status(400)
+          .send(`Webhook Error: ${(error as Error).message}`);
+      }
+
+      if (event!.type === "checkout.session.completed") {
+        const { customer_details } = event.data
+          .object as Stripe.Checkout.Session;
+
+        if (!customer_details?.email) {
+          console.error("Failed to find customer_details", customer_details);
+        }
+        try {
+          await sendOrderConfirmationEmail(customer_details!.email!);
+          return response
+            .status(200)
+            .send(`Confirmation email sent to ${customer_details?.email}`);
+        } catch (error: any) {
+          console.error(
+            "Failed to send order confirmation email:",
+            error.message
+          );
+        }
+      }
+    } catch (error) {}
+  }
+);
 
 // Middleware
 app.use(
